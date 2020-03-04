84e55be56dcd98a5ac044301a6f65f38880dd111
adz5a <antoine.d.zimmermann@gmail.com>
2020-03-04 15:25:25 +0100

    Remove devtool

--- src/react_tools/devtool.cljs
+++ src/react_tools/devtool.cljs
@@ -1,29 +1,10 @@
 (ns react-tools.devtool
   (:require [react]
             [react-dom]
-            [goog.object :as gobject])
+            [goog.object :as gobject]
+            [react-tools.component])
 
   (:require-macros [react-tools.component :refer [defcomponent jsx]]))
-
-
-(defn DevToolPortal
-  [props]
-  (let [dom-element (react/useRef (.createElement js/document "div"))
-        insert-dom-element-effect (react/useEffect (fn []
-                                                     (let [dev-tool-container (.getElementById js/document "react-tools-dev-tool-container")
-                                                           has-container? (not (nil? dev-tool-container))]
-                                                       (if has-container?
-                                                         (.appendChild dev-tool-container (.-current dom-element))
-                                                         (let [dev-tool-container (.createElement js/document "div")]
-                                                           (.appendChild (.-body js/document) dev-tool-container)
-                                                           (gobject/set dev-tool-container "id" "react-tools-dev-tool-container")
-                                                           (.setAttribute dev-tool-container "style" "position: fixed; top: 0; background: white; left: 0; display: inline-block;")
-                                                           (.appendChild dev-tool-container (.-current dom-element)))))
-                                                     js/undefined)
-                                                   (array))]
-    (react-dom/createPortal
-      (.-children props)
-      (.-current dom-element))))
 
 (defcomponent DevTool
   [props]
